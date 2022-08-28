e03e8fde5c2faab84394f215303e9f907962a75e
Jan Kočvara <kimichisxd285@gmail.com>
2022-08-28 02:22:51 +0200

    introduce manual database status checker middleware

---
+++ Middlewares/DatabaseStatusMiddleware.cs
@@ -0,0 +1,25 @@
+using dchv_api.Database;
+
+namespace dchv_api.Middlewares
+{
+    public static class DatabaseStatusMiddleware 
+    {
+        public static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DatabaseContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+    }
+}
